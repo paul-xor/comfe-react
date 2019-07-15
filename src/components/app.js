890f82b3441b44767cdce729f3b5afd29a25707f
Pasha Smolov <palexsmol@gmail.com>
2019-07-15 16:50:07 -0700

    check login status

--- src/components/app.js
+++ src/components/app.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import Dashboard from './Dashboard';
 import Home from './Home';
+import axios from 'axios';
+
 
 export default class App extends Component {
   constructor(){
@@ -13,6 +15,30 @@ export default class App extends Component {
     };
 
     this.handleLogin = this.handleLogin.bind(this)
+  }
+
+  checkLoginStatus(){
+    axios.get("http://localhost:3005/logged_in", { withCredentials: true })
+    .then( response => {
+      if (response.data.logged_in && this.state.loggedInStatus === "NOT_LOGGED_IN"){
+        this.setState({
+          loggedInStatus: "LOGGED_IN",
+          user: response.data.user
+        })
+      } else if (!response.data.logged_in & this.state.loggedInStatus === "LOGGED_IN"){
+        this.setState({
+          loggedInStatus: "NOT_LOGGED_IN",
+          user: {}
+        })
+      }
+    })
+    .catch(error => {
+      console.log("check login error", error);
+    });
+  }
+
+  componentDidMount(){
+    this.checkLoginStatus();
   }
 
   handleLogin(data) {
